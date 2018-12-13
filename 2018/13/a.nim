bbbfd9a5addab482b5b5f9e5574d6871eb42ffbe
Reeder <connor.reeder@bulletproofsi.com>
2018-12-13 16:24:14 -0400

    working on day 12

---
+++ 2018/13/a.nim
@@ -0,0 +1,15 @@
+import streams
+type
+  Rail = ref object
+    next, prev: Rail
+    x, y: uint
+    cart: bool
+  Intersection = seq[Rail]
+ 
+iterator ilines(fs: FileStream): (uint, string)=
+  var i: uint
+  for ln in fs.lines:
+    yield (i, ln)
+    inc(i)
+
+discard initRail("input")
