cfb787ed12e25e62e561b4bd6511f539ee2f751e
syril <syril@tfwno.gf>
2019-11-01 17:08:17 -0300

    cool

--- 2018/13/a.nim
+++ 2018/13/a.nim
@@ -35,7 +35,7 @@ template search(d: static[Direction]): untyped =
 var
   carts: seq[Cart]
   rails: seq[Rail]
-block:
+when true:
   var raw = toSeq(lines("2018/13/input"))
   let
     height = len(raw).Nt
@@ -52,22 +52,62 @@ block:
         add(rails, r)
     raw[sy] = ""
 
-template tick(): untyped =
-  # THIS IS WRONG!! ITER OVER RAILS
-  for c in carts:
-    case c.dir
-    of right:
-      c.x += 1
+template quick(dcmp; cmp, wt1,rs1, wt2,rs2): untyped =
+  when dir==dcmp:(if cmp==wt1:c.dir=rs1)else:(if cmp==wt2:c.dir=rs2)
 
-    of down:  c.y += 1
-    of left:  c.x -= 1
-    of up:    c.y -= 1
+template turnIf(d: static[Direction]): untyped {.dirty.} =
+  const dir = d
+  when dir in {right, left}:
+    if c.y == r.y1:
+          quick(left,  c.x, r.x1,down, r.x2,up) #top
+    else: quick(right, c.x, r.x1,up,   r.x2,down) #bottom
+  else:
+    if c.x == r.x1:
+          quick(up,    c.y, r.y1,right, r.y2,left) #left
+    else: quick(down,  c.y, r.y1,left,  r.y2,right) #right
+
+template tick(): untyped {.dirty.} =
+  bind rails
+  block:
+    bind carts
+    let c = carts[0]
+    echo domain(c.x.int,c.y.int,raw)
+  for r in rails:
+    for c in r.carts:
+      case c.dir
+      of right:
+        c.x += 1; turnIf(right)
+      of down:  c.y += 1; turnIf(down)
+      of left:  c.x -= 1; turnIf(left)
+      of up:    c.y -= 1; turnIf(up)
+
+proc domain(x,y: int; s: var seq[string]; r: int = 1): seq[string] =
+  let
+    ln: int = r.int*2+1
+    df: int = r*2
+  result = newSeqOfCap[string](ln)
+  for i in 0..<ln:
+    add(result, newStringOfCap(ln))
+  for i in y-r..<y+r:
+    for j in x-r..<x+r:
+      echo i, ' ', j
+      result[i][j] = raw[i-df][j-df]
 
 template collision(): untyped =
+  bind carts
   let lc = len(carts)
-  for i,c1 in 0..<lc:
-    for c2 in (i+1)..<lc:
+  var i: int
+  while i < lc:
+    let c1 = carts[i]
+    var j = i+1
+    while j < lc:
+      let c2 = carts[j]
       if c1.x==c2.x and c1.y==c2.y:
-        echo "COLLISION AT "&c1.x&", "&c1.y
+        echo "COLLISION AT " & $c1.x & ", " & $c1.y
+        #quit(0)
+      inc j
+    inc i
 
-#underway. finish tick. collission done?
+while true:
+  tick()
+  collision()
