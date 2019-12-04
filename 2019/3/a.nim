8e0b4c2a332e43dbe8b8cb477641051f15dafb41
syril <syril@tfwno.gf>
2019-12-04 17:20:33 -0400

    still on 3

--- 2019/3/a.nim
+++ 2019/3/a.nim
@@ -6,23 +6,24 @@ type
   Direction = enum
     down='D', left='L', right='R', up='U'
   Point = tuple[x, y: int]
-  Line = array[2, Point]
   WirePath = seq[Point]
 
-proc intersect(a, b: Line): bool =
-  a[0].x <= b[1].x and a[1].x >= b[0].x and
-  a[0].y <= b[1].y and a[1].y >= b[0].y
+proc intersect(a0,a1, b0,b1: Point): bool =
+  (a1.x <= b0.x and a0.x >= b1.x and
+   a1.y <= b0.y and a0.y >= b1.y) or
+  (a1.x >= b0.x and a0.x <= b1.x and
+   a1.y >= b0.y and a0.y <= b1.y)
 
-proc xy(wp: WirePath; dir: Direction; mov: int): Point =
+proc xy(wp: WirePath; dir: Direction; m: int): Point =
   let l = wp[^1]
   case dir
-  of up:    (l.x, l.y + mov)
-  of right: (l.x + mov, l.y)
-  of down:  (l.x, l.y - mov)
-  of left:  (l.x - mov, l.y)
+  of up:    (l.x, l.y + m)
+  of right: (l.x + m, l.y)
+  of down:  (l.x, l.y - m)
+  of left:  (l.x - m, l.y)
 
 var wps: array[2, WirePath]
-for i,ln in ilines(input):
+for i,ln in ilines(input("1")):
   var wp: WirePath
   add(wp, (x: 0, y: 0))
   for ins in split(ln, ','):
@@ -32,20 +33,26 @@ for i,ln in ilines(input):
     add(wp, xy(wp, dir, mov))
   wps[i] = wp
 
-echo wps[0]
-
-var
-  rs: seq[Point]
-  p1 = wps[0][0]
+echo wps
+echo len(wps[1])
+var (a0,a1, dis) = (wps[0][0], Point (0,0), high(int))
 for i in 1..<len(wps[0]):
-  let l1 = Line [ p1, wps[0][i] ]
-  p1 = l1[1]
-  var p2 = wps[1][0]
-  for j in i..<len(wps[1]):
-    let l2 = Line [ p2, wps[1][j] ]
-    p2 = l2[1]
-    if l1[0].x in l2[0].x .. l2[1].x:
-
-    else
-    #if intersect(l1, l2):
-    #  echo "cool"
+  a1 = wps[0][i]
+  var (b0,b1) = (wps[1][0], Point (0,0))
+  for j in 1..<len(wps[1]):
+    b1 = wps[1][j]
+    if intersect(a0,a1, b0,b1):
+      let (x, y) = if a0.x == a1.x:
+        (a1.x, b1.y)
+      else:
+        (b1.x, a1.y)
+      let ndis = abs(x) + abs(y)
+      if ndis == 0: continue
+      echo "Intersection at ", x, ", ", y, ". distance ", ndis
+      if ndis < dis:
+        echo "New lower distance: ", ndis
+        dis = ndis
+    b0 = b1
+  a0 = a1
+# 142 too low
+echo dis
